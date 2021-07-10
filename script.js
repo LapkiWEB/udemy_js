cbcb48ccbd136248bef313800c7f890f081721e6
Stepan <Stefan.irk96@mail.ru>
2021-07-10 09:37:02 +0800

    lesson 24 my version

--- script.js
+++ script.js
@@ -1,68 +1,83 @@
 "use strict";
+
 let numberOfFilms;
-
-function start() {
-     numberOfFilms = +prompt("Сколько фильмов вы уже посомтрели?", "");
-
-     while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посомтрели?", "");
-     }
-}
-
-start();
 
 const personalMovieDB = {
     count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
-    privat: false
-};
+    privat: true,
+    startTest: function () {
+        numberOfFilms = +prompt("Сколько фильмов вы уже посомтрели?", "");
 
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из последних просмотренных фильмов?", ""),
-            b = prompt("На сколько оцените его?", "");
-        if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
-            console.log('done');
-            personalMovieDB.movies[a] = b;
+        while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = +prompt("Сколько фильмов вы уже посомтрели?", "");
+        }
+        personalMovieDB.count = numberOfFilms;
+    },
+    rememberMyFilms: function () {
+        for (let i = 0; i < 2; i++) {
+            const a = prompt("Один из последних просмотренных фильмов?", ""),
+                b = prompt("На сколько оцените его?", "");
+            if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+                console.log('done');
+                personalMovieDB.movies[a] = b;
+            } else {
+                console.log('error');
+                i--;
+            }
+        }
+    },
+    detectPersonalLevel: function () {
+        if (personalMovieDB.count < 10) {
+            console.log("Просмотренно довольно мало фильмов!");
+        } else if (personalMovieDB.count < 30) {
+            console.log("Вы классический зритель");
+        } else if (personalMovieDB.count > 30) {
+            console.log("Вы киноман");
         } else {
-            console.log('error');
-            i--;
+            console.log("Ошибка!");
+        }
+    },
+    showMyDB: function (hidden) {
+        if (!hidden) {
+            console.log(personalMovieDB);
+        }
+    },
+    writeYourGenres: function writeYourGenres() {
+
+        for (let i = 1; i <= 3; i++) {
+            personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
+            if (personalMovieDB.genres[i - 1] == '' || personalMovieDB.genres[i - 1] == null) {
+                i--;
+            }
+
+
+        }
+
+    },
+    toggleVisibleMyDB: function () {
+        if (personalMovieDB.privat == false) {
+            personalMovieDB.privat = true;
+        } else {
+            personalMovieDB.privat = false;
         }
     }
-}
+};
 
-rememberMyFilms();
+personalMovieDB.startTest();
+personalMovieDB.rememberMyFilms();
+personalMovieDB.detectPersonalLevel();
 
+personalMovieDB.toggleVisibleMyDB();
 
-function detectPersonalLevel(){
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотренно довольно мало фильмов!");
-    } else if (personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count > 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Ошибка!");
-    }
-}
-
-detectPersonalLevel();
+personalMovieDB.showMyDB(personalMovieDB.privat);
+personalMovieDB.writeYourGenres();
 
 
-function showMyDB(hidden) {
-    if (!hidden){
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat);
+const copy = [...personalMovieDB.genres];
 
-function writeYourGenres () {
-    for(let i = 1; i <= 3; i++){
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`);
-    }
-}
-writeYourGenres();
-
+personalMovieDB.genres.forEach(function (item, i, copy) {
+    console.log(`Любимый жанр №${i+1} - это ${item}`);
+});
